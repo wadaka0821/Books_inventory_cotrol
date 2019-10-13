c541d8c1e2c021880fa821c86dc1ef4d7403ccbf
wadaka0821 <wadaka0821@gmail.com>
2019-10-13 20:11:19 +0900

    create fundamental file

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 
-menus = {1:"在庫表示",2:"在庫情報の編集",3:"終了"}
+menus = {"1":"在庫表示","2":"在庫情報の編集","3":"終了"}
 
 while True:
     print("-----------在庫管理システム-----------")
     print("-----------基本操作メニュー-----------")
     for i,j in menus.items():
-        print(str(i).ljust(3),"|",j.center(22))
-    input()
+        print(i.ljust(3),"|",j.center(22))
+
+    select = input("操作したいオプション番号を入力してください")
+
+    if i == "1":
+        pass
+    elif i == "2":
+        pass
+    elif i == "3":
+        break
+    else:
+        print("入力された番号に誤りがあります")
