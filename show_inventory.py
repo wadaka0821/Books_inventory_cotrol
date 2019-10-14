90891dac0296728f044a66bf0dbe410071bed750
wadaka0821 <wadaka0821@gmail.com>
2019-10-14 18:04:26 +0900

    finish show_all in show_inventory.py

--- show_inventory.py
+++ show_inventory.py
@@ -1,4 +1,5 @@
 import csv
+import copy
 
 #ソートオプション
 #カテゴリー
@@ -6,12 +7,13 @@ import csv
 #商品名
 class Show_inventory:
     def __init__(self):
-        self.products = list()
+        self.all_products = list()
+        self.conditioned_products = list()
         self.menu()
 
     def menu(self):
         self.road_current()
-        self.show_all()
+        self.show()
         while True:
             print("-----------在庫表示-----------")
             print("------------------------------")
@@ -21,7 +23,11 @@ class Show_inventory:
         with open("books_info/current.csv","r") as f:
             reader = csv.reader(f)
             for i in reader:
-                self.products.append([i[0],i[1],int(i[2])])
+                self.all_products.append([i[0],i[1],int(i[2])])
+        self.conditioned_products = copy.deepcopy(self.all_products)
 
-    def show_all(self):
-        pass
+    def show(self):
+        print("-"*50)
+        print("{:-^14}{:-^20}{:-^7}".format("商品番号","商品名","個数"))
+        for i in self.conditioned_products:
+            print("{0[0]:^18}|{0[1]:^20}|{0[2]:>7}|".format(i))
