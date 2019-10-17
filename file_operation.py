cef30c2d03459f6b5006d437970f098e84dabb57
wadaka0821 <wadaka0821@gmail.com>
2019-10-17 13:06:05 +0900

    update file_operaionl.py

--- file_operation.py
+++ file_operation.py
@@ -45,7 +45,6 @@ class File_operation:
         with open(self.input_filename,"r") as f:
             reader = csv.reader(f)
             warning = list()
-            #在庫数が負になっても警告なし
             for i in reader:
                 if i[0] in self.current:
                     self.current[i[0]][1] += int(i[2])
@@ -73,6 +72,20 @@ class File_operation:
             self.writerow_file(adding_list,"books_info/current.csv","w")
             self.writerow_file(add_list,"books_info/time_log/add.csv","a")
             self.writerow_file(sub_list,"books_info/time_log/sub.csv","a")
+
+            print("正常にファイルの読み込みが完了しました")
+            while True:
+                select = input("読み込みを行ったファイルの内容を破棄しますか？（yes or no）（推奨）")
+                if select == "yes":
+                    with open(self.input_filename,"w") as f:
+                        writer = csv.writer(f)
+                        writer.writerow(list())
+                    break
+                elif == "no":
+                    print("ファイル内容を保持しました")
+                    break
+                else:
+                    print("正しい入力をしてください")
 
     def road_current(self):
         with open("books_info/current.csv","r") as f:
